84da5fa79a11bcc7569a5d8999c300b2d8e196a2
Rafael Bizao <rabizao@gmail.com>
2022-04-06 21:13:51 -0300

    better error message for invalid token

--- lablift_client/client.py
+++ lablift_client/client.py
@@ -10,9 +10,9 @@ class Client:
             kwargs["headers"] = {}
         kwargs["headers"]["Authorization"] = "Bearer " + self.token
         response = getattr(requests, method)(route, **kwargs)
+        if response.status_code == 401:
+            raise Exception("Not authorized! Please verify if your token is correct and if you have access to this service.")
         if not response:
             raise Exception(
                 f"[Error] Cannot query server for route {route}. {response.content}")
-        if response.status_code == 401:
-            raise Exception("Invalid token!")
         return response
